e489dbcd344812e7f139bfe7983d724f5a7501b2
OlgaTsyganova <superolk@mail.ru>
2021-06-05 16:32:36 +0300

    second homework

--- js/main.js
+++ js/main.js
@@ -1,19 +1,19 @@
 function getRandom (min,max) {
   {
-    if (max >= min) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-   }
-   return "Диапaзон может быть только положительным, включая ноль"
+  if (max >= min) {
+      min = Math.ceil(min);
+      max = Math.floor(max);
+      return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+    return 'Диапaзон может быть только положительным, включая ноль';
   }
 }
-console.log(getRandom (9,8));
+getRandom (9,8);
 
 function checkCommentLength (stringLength, maxLength) {
   if (stringLength <= maxLength) {
     return true;
   }
-return false;
-};
-console.log(checkCommentLength(50, 140));
+  return false;
+}
+checkCommentLength(50, 140);
