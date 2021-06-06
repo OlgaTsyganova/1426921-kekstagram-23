16db6545cd63ede06890a59f73cae1c4f842e4a5
OlgaTsyganova <superolk@mail.ru>
2021-06-06 17:26:41 +0300

    Fixed errors in second homework

--- js/main.js
+++ js/main.js
@@ -1,19 +1,14 @@
 function getRandom (min,max) {
-  {
-    if (max >= min) {
-      min = Math.ceil(min);
-      max = Math.floor(max);
-      return Math.floor(Math.random() * (max - min + 1)) + min;
-    }
-    return 'Диапaзон может быть только положительным, включая ноль';
+  if (min < max) {
+    min = min >= 0 ? min : 0;
+    return Math.floor(Math.random() * (max - min + 1) + min);
   }
+  return 'Диапaзон может быть только положительным, включая ноль';
 }
+
 getRandom (9,8);
 
-function checkCommentLength (stringLength, maxLength) {
-  if (stringLength <= maxLength) {
-    return true;
-  }
-  return false;
+function checkStringMaxLength (string, maxLength) {
+  return string.length <= maxLength;
 }
-checkCommentLength(50, 140);
+checkStringMaxLength('I am going to eat', 140);
